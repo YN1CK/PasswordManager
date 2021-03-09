630a0ee8335c4b650c0e362ce204254690e57b38
YN1CK <yannick.reiss@gmx.de>
2021-03-09 09:21:45 +0100

    Basic Program with Window.

---
+++ main.py
@@ -0,0 +1,25 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+import sys
+
+
+class Main(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.browser = QTextBrowser(self)
+        self.resize(1000, 750)
+        self.show()
+        self.set_layout()
+
+    def set_layout(self):
+        self.browser.move(100, 100)
+        self.browser.resize(100, 300)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = Main()
+    sys.exit(app.exec_())
